a213485c68951d20bf700556faef85524b302819
arcadiafalcone <arcadia@stanford.edu>
2020-01-07 13:24:28 -0800

    Merges TermLookup class into AuthorityLookup class

--- apps/authority_lookup/authority_lookup.rb
+++ apps/authority_lookup/authority_lookup.rb
@@ -1,30 +1,59 @@
-require_relative 'term_lookup'
-require_relative 'result_parser'
+require 'cgi'
+require 'net/http'
+require 'json'
 
 class AuthorityLookup
 
-  attr_reader :result_set
+  attr_reader :subauthority, :limit, :language, :exit
 
-  def initialize(term_list, authority, base_url, outfile, subauthority: '', limit: 10, language: 'en')
+  def initialize(term_list, authority, base_url, subauthority: '', limit: 10, language: 'en')
     @term_list = term_list
     @authority = authority
     @base_url = base_url
-    @outfile = outfile
     @subauthority = subauthority
     @limit = limit
     @language = language
+    @exit = false
 
-    @result_set = lookup_terms
-    ResultParser.new(@result_set, @outfile)
+    @exit = true if @term_list == nil || @term_list == []
+    @exit = true if @authority == nil || @authority == ""
+    @exit = true if @base_url == nil || @base_url == ""
+
+    return if @exit
+
   end
 
-  def lookup_terms
+  def process_term_list
     result_set = []
-    @term_list.each do |term|
-      result_set << TermLookup.new(term, @authority, @base_url, @subauthority, @limit, @language).result
+    @term_list.each do |search_term|
+      next if search_term == nil || search_term.empty?
+      result = lookup_term(search_term)
+      result_set << result
     end
     result_set
   end
 
+  def lookup_term(search_term)
+    encoded_search_term = encode_search_term(search_term)
+    query_url = construct_query(encoded_search_term)
+    response = run_query(query_url)
+    parsed_response = JSON.parse(response)
+    result = {search_term => parsed_response}
+  end
+
+  def encode_search_term(search_term)
+    encoded_search_term = CGI::escape(search_term)
+  end
+
+  def construct_query(encoded_search_term)
+    query_url = "#{@base_url}#{@authority}"
+    query_url += "/#{@subauthority}" unless @subauthority == nil || @subauthority.empty?
+    query_url += "?q=#{encoded_search_term}&maxRecords=#{@limit}&lang=#{@language}"
+  end
+
+  def run_query(query_url)
+    query = URI(query_url)
+    Net::HTTP.get(query)
+  end
 
 end
--- spec/apps/authority_lookup/authority_lookup_spec.rb
+++ spec/apps/authority_lookup/authority_lookup_spec.rb
@@ -3,11 +3,61 @@ require '../apps/authority_lookup/file_parser'
 
 RSpec.describe AuthorityLookup do
 
-  describe 'looks up terms' do
-    it 'returns results for each term' do
-      term_list = FileParser.new('./fixtures/lookup_list.txt').terms
-      results = AuthorityLookup.new(term_list, 'LOCNAMES_LD4L_CACHE', 'https://lookup.ld4l.org/authorities/search/linked_data/', 'outfile').result_set
-      expect(result_set.size).to eq(2)
+  before(:all) do
+    @authority_lookup_test = AuthorityLookup.new(['Dorothy Dunnett'], 'LOCNAMES_LD4L_CACHE', 'https://lookup.ld4l.org/authorities/search/linked_data/')
+  end
+
+  describe 'exits if arguments missing:' do
+    it 'exits if term list not provided' do
+      expect(AuthorityLookup.new([], 'authority', 'http://example.com/').exit).to eq(true)
+    end
+    it 'exits if authority not provided' do
+      expect(AuthorityLookup.new(['term'], '', 'http://example.com/').exit).to eq(true)
+    end
+    it 'exits if base URL not provided' do
+      expect(AuthorityLookup.new(['term'], 'authority', nil).exit).to eq(true)
     end
   end
+
+  describe 'parses optional arguments:' do
+    it 'sets subauthority' do
+      subauthority_option = AuthorityLookup.new(['term'], 'authority', 'http://example.com/', subauthority: 'naf')
+      expect(subauthority_option.subauthority).to eq('naf')
+    end
+    it 'sets limit' do
+      limit_option = AuthorityLookup.new(['term'], 'authority', 'http://example.com/', limit: 1)
+      expect(limit_option.limit).to eq(1)
+    end
+    it 'sets language' do
+      language_option = AuthorityLookup.new('term', 'authority', 'http://example.com/', language: 'ru')
+      expect(language_option.language).to eq('ru')
+    end
+  end
+
+  describe 'prepares query:' do
+    it 'encodes the search term' do
+      expect(@authority_lookup_test.encode_search_term('Dorothy Dunnett')).to eq('Dorothy+Dunnett')
+    end
+    it 'constructs a basic query' do
+      expect(@authority_lookup_test.construct_query('Dorothy+Dunnett')).to eq('https://lookup.ld4l.org/authorities/search/linked_data/LOCNAMES_LD4L_CACHE?q=Dorothy+Dunnett&maxRecords=10&lang=en')
+    end
+    it 'constructs a query with options' do
+      options_test = AuthorityLookup.new('Dorothy Dunnett', 'LOCNAMES_LD4L_CACHE', 'https://lookup.ld4l.org/authorities/search/linked_data/', subauthority: 'naf', limit: 1, language: 'ru')
+      expect(options_test.construct_query('Dorothy+Dunnett')).to eq('https://lookup.ld4l.org/authorities/search/linked_data/LOCNAMES_LD4L_CACHE/naf?q=Dorothy+Dunnett&maxRecords=1&lang=ru')
+    end
+  end
+
+  describe 'returns results:' do
+    it 'returns a result for a search term' do
+      lookup_result = @authority_lookup_test.lookup_term('Dorothy Dunnett')
+      expect(lookup_result['Dorothy Dunnett']).not_to eq([])
+    end
+    it 'returns results for each term in a list' do
+      term_list = FileParser.new('./fixtures/lookup_list.txt').terms
+      expect(term_list.size).to eq(2)
+      results = AuthorityLookup.new(term_list, 'LOCNAMES_LD4L_CACHE', 'https://lookup.ld4l.org/authorities/search/linked_data/').process_term_list
+      expect(results.size).to eq(2)
+    end
+  end
+
 end
