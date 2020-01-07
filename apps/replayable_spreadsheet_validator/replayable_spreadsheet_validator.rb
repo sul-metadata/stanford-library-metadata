0fdc2804ad4e85cd1958b707d3d5f67602ea454b
arcadiafalcone <arcadia@stanford.edu>
2020-01-07 14:55:24 -0800

    Change validator template reference to absolute path

--- apps/replayable_spreadsheet_validator/replayable_spreadsheet_validator.rb
+++ apps/replayable_spreadsheet_validator/replayable_spreadsheet_validator.rb
@@ -6,7 +6,7 @@ class Validator
 
   def initialize(filename)
     @filename = filename
-    @template = './apps/replayable_spreadsheet_validator/modsulator_template.xml'
+    @template = File.expand_path('./modsulator_template.xml', File.dirname(__FILE__))
     @extension = File.extname(@filename)
     @report = CSV.new(File.open('./public/rps_validator/report.csv', 'w'))
 
