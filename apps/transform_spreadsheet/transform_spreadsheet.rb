c1e9fab806e38f14d53d5c819e9c94e97e5b6fe2
arcadiafalcone <arcadia@stanford.edu>
2020-01-08 10:41:51 -0800

    Simplify file IO for Transformer

--- apps/transform_spreadsheet/transform_spreadsheet.rb
+++ apps/transform_spreadsheet/transform_spreadsheet.rb
@@ -3,6 +3,8 @@ require 'csv'
 
 class Transformer
 
+  attr_reader :map_data, :string_data, :complex_data, :data_rules, :output_order, :exit
+
   def initialize(in_filename, map_filename, out_filename)
     @in_filename = in_filename
     @map_filename = map_filename
@@ -20,25 +22,27 @@ class Transformer
 
     # Order of target fields in mapfile, to use for output
     @output_order = []
+
+    @exit = false
   end
 
   def transform
-    process_mapfile
-    process_data
+    process_mapfile(@map_filename)
+    process_data(@in_filename, @out_filename)
   end
 
-  def fail_error(msg)
-    puts "FAIL: #{msg}"
-    exit
-  end
-
-  def process_mapfile
-    mapping = case File.extname(@map_filename)
-    when '.csv' then Roo::Spreadsheet.open(@map_filename, extension: :csv)
-    when '.xls' then Roo::Spreadsheet.open(@map_filename, extension: :xls)
-    when '.xlsx' then Roo::Spreadsheet.open(@map_filename, extension: :xlsx)
-    else fail_error("Invalid input file extension: use .csv, .xls, or .xlsx")
+  def open_spreadsheet(filename)
+    spreadsheet = case File.extname(filename)
+    when '.csv' then Roo::Spreadsheet.open(filename, extension: :csv)
+    when '.xls' then Roo::Spreadsheet.open(filename, extension: :xls)
+    when '.xlsx' then Roo::Spreadsheet.open(filename, extension: :xlsx)
+    else @exit = true
     end
+    return spreadsheet
+  end
+
+  def process_mapfile(filename)
+    mapping = open_spreadsheet(filename)
 
     mapping.each do |fields|
       # Convert all values to strings
@@ -56,18 +60,13 @@ class Transformer
     end
   end
 
-  def process_data
-    spreadsheet = case File.extname(@in_filename)
-    when '.csv' then Roo::Spreadsheet.open(@in_filename, extension: :csv)
-    when '.xls' then Roo::Spreadsheet.open(@in_filename, extension: :xls)
-    when '.xlsx' then Roo::Spreadsheet.open(@in_filename, extension: :xlsx)
-    else fail_error("Invalid input file extension: use .csv, .xls, or .xlsx")
-    end
+  def process_data(in_filename, out_filename)
+    spreadsheet = open_spreadsheet(in_filename)
 
     # Open output file
-    @outfile = CSV.open(@out_filename, 'wb')
+    outfile = CSV.open(out_filename, 'wb')
     # Write target headers to output
-    @outfile << @output_order
+    outfile << @output_order
 
     # Get source headers for data field indexes
     data_fields = spreadsheet.row(1)
@@ -78,35 +77,35 @@ class Transformer
       # Convert all values to strings
       row.map! {|x| x.to_s}
       # Populate row hash with constant string data from mapfile
-      @data_out = Hash.new.merge(@string_data)
+      data_out = Hash.new.merge(@string_data)
       # Add source data to row hash based on simple mapping
       @map_data.each do |target, source|
-        @data_out[target] = row[data_fields.index(source)] if data_fields.include?(source)
+        data_out[target] = row[data_fields.index(source)] if data_fields.include?(source)
       end
       # Add source data to row hash incorporating {variables}
       @complex_data.each do |target, source|
         # Generate data only if all variable names are also present in data field names
-        @data_out[target] = source.gsub(/{[^}]*}/) {|s| row[data_fields.index(s[1..-2])]} if source.scan(/{([^}]*)}/).flatten - data_fields == []
+        data_out[target] = source.gsub(/{[^}]*}/) {|s| row[data_fields.index(s[1..-2])]} if source.scan(/{([^}]*)}/).flatten - data_fields == []
       end
       # Delete data from row hash when required dependency is not present
       @data_rules.each do |target, rule|
         if data_fields.index(rule) == nil || row[data_fields.index(rule)] == nil || row[data_fields.index(rule)] =~ /^\s*$/
-          @data_out[target] = nil
+          data_out[target] = nil
         end
       end
-      output_row_data
+      outfile << output_row_data(data_out)
     end
-    @outfile.close
+    outfile.close
   end
 
-  def output_row_data
+  def output_row_data(row)
     # Ordered array for output data
     row_out = []
     ## Write data to output
     @output_order.each do |field|
-      if @data_out[field] != nil
+      if row[field] != nil
         # Remove newlines and extra spaces from within cell values
-        data = @data_out[field].gsub(/[\r\n]/," ").gsub(/\s+/," ").strip
+        data = row[field].gsub(/[\r\n]/," ").gsub(/\s+/," ").strip
         # Add cleaned data to output array
         row_out << data
       else
@@ -114,8 +113,7 @@ class Transformer
         row_out << ""
       end
     end
-    # Write row to output
-    @outfile << row_out
+    return row_out
   end
 
 end
