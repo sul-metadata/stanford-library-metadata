101e3fb7f4cd6a96a1f725dce79711984317eb44
arcadiafalcone <arcadia@stanford.edu>
2020-01-13 11:30:57 -0800

    Set fixtures directory as FIXTURES_DIR constant

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'equivalent-xml/rspec_matchers'
 
+FIXTURES_DIR = File.expand_path('fixtures', __dir__)
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
